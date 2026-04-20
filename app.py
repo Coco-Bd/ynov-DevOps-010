3a96764aa8263fb6c78f09c38866d2f0fa81bfae
Corentin <ccbd31@gmail.com>
2026-04-20 15:06:48 +0200

    feat: add prom metrics and request uuid

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 from flask import Flask, g
 from uuid import uuid4
-app = Flask(__name__)
+from prometheus_client import Counter, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
+app = Flask(__name__)
+http_requests_total = Counter('http_requests_total', 'Total HTTP requests', ['status'])
+http_requests_all_total = Counter('http_requests_all_total', 'Total HTTP requests')
 @app.route("/")
 def landing_page():
     return "App Flask. Permet de generer les metriques pour le projet \n - /error = err 500\n - /ok = OK 200"
 
 @app.route("/ok")
 def return_valid():
+    http_requests_total.labels(status='200').inc()
     return "OK", 200
 
 @app.route("/error")
 def return_error():
+    http_requests_total.labels(status='500').inc()
     return "err", 500
 
-@app.route("/metrics")
-def return_metrics():
-    return "metrics"
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 @app.before_request
 def before_request():
   g.request_id = str(uuid4())
+  http_requests_all_total.inc()
 
 
 
